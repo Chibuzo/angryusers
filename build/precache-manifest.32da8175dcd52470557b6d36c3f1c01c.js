850860c3df71755dfee17007999da5189a084be3
Chibuzo <chibuzo.henry@gmail.com>
2019-03-15 13:16:04 +0100

    Added build folder to repo

---
+++ build/precache-manifest.32da8175dcd52470557b6d36c3f1c01c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "158dd0acac8e7ede33f645cd4aa215e2",
+    "url": "/static/media/slide.158dd0ac.jpg"
+  },
+  {
+    "revision": "2a3f6354fbce3304d50b5809fd9a767a",
+    "url": "/static/media/footer-logo.2a3f6354.png"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "1280185654fec0fd80e0",
+    "url": "/static/js/main.12801856.chunk.js"
+  },
+  {
+    "revision": "578968dd29fcaa798343",
+    "url": "/static/js/2.578968dd.chunk.js"
+  },
+  {
+    "revision": "1280185654fec0fd80e0",
+    "url": "/static/css/main.dce1c0e2.chunk.css"
+  },
+  {
+    "revision": "578968dd29fcaa798343",
+    "url": "/static/css/2.f0045289.chunk.css"
+  },
+  {
+    "revision": "98b636dee0a3796c0102092841b2bc31",
+    "url": "/index.html"
+  }
+];
